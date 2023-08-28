e43de2958d8d6110ccfac9a4b3a92574fc741de8
FridaGathima <frida.gathima@student.moringaschool.com>
2023-08-28 23:00:15 +0300

    edited restaurant

--- lib/restaurant.py
+++ lib/restaurant.py
@@ -5,14 +5,7 @@ class Restaurant(Review):
     # restaurant initialization
     def __init__(self, name): # def __init__(self, restaurant_name)
         self.name = name
-        super().__init__(self, name)
-        # self.rating = rating
-        # self.customer = customer
-
-        # super().review_all()
-        # # self.rating = rating
-    
-    # returns restaurant name
+       
     @property
     def name(self):
         return self.name
@@ -21,14 +14,11 @@ class Restaurant(Review):
     def reviews(self):
         reviews = Review.all
         return [rev for rev in reviews if rev.restaurant.name == self.name]
-    #  return [rev for rev in reviews if rev.restaurant.name() == self.name]
 
     # Returns a **unique** list of all customers who have reviewed a particular restaurant.
     def customers(self):
         review = Review.all
         return list({rev.customer for rev in review if rev.restaurant.name == self.name })
-        #super().customer()
-        #return self.rating
     
     # returns the average star rating for a restaurant based on its reviews
 
